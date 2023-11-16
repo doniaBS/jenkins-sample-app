5ef2700631780e9e810edd49703722271c44ff54
DOniaBS <bensedrinedonia@gmail.com>
2023-11-16 15:54:48 +0100

    first commit

---
+++ sample_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
